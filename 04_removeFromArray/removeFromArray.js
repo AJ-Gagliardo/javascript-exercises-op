d2991015fa51adf98f2076b9fb1527a1e16e9b1c
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-12-16 22:12:46 -0800

    putting my own exercises here and some from websites just to practice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,9 @@
-const removeFromArray = function() {
-
-};
+const removeFromArray = function(list, toDelete){
+    const index= list.indexOf(toDelete);
+    list.splice(index,1);
+    return list;
+  
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;
