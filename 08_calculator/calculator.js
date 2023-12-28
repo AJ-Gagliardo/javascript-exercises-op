f3332ae5d2b5a5a9f090cd1a27421b40b8315794
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-12-27 18:21:17 -0800

    finished calculator exercises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+
+	return numbers.reduce((previousSum, newNumber)=> previousSum + newNumber, 0);
+} ;
+
+
+
+const multiply = function(numbers) {
+if (numbers.length === 0){
+  return 0
+}
+
+  return numbers.reduce((previous, newNumber)=> previous * newNumber, 1)
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+	// return numbers.reduce((previous, newNumber) => previous ** newNumber)
+  return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function (n) {
+  if (n === 0) return 1;
+  let product = 1;
+  for (let i = n; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
