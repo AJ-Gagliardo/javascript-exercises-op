e071ac8b8c5b613fbea0feabb90a2132ee9f8089
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-12-27 19:43:57 -0800

    finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,22 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    wordsClean = words.replace(/[^a-zA-Z0-9]/g, "").toLowerCase()
+    let reverse = wordsClean.split('').reverse().join('');
+   return wordsClean === reverse
+    }
 
-};
+;
+
+
+
+//brainstorm and pseudocode
+/*
+ First need to  join the whole string 
+
+ 
+
+
+*/
+
 
 // Do not edit below this line
 module.exports = palindromes;
